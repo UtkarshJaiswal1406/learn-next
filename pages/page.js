b670274694196cecc2e899c7b8cd5770186f7f56
vaisaliOH <vaisalinair05@gmail.com>
2024-10-28 23:41:07 +0530

    Added pages

---
+++ pages/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyPage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-50">
+      <h1 className="text-4xl font-bold text-blue-500">Welcome to My Page!</h1>
+    </div>
+  );
+};
+
+export default MyPage;
