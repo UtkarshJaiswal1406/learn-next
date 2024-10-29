6d78793e3b4a2ace44d776b1f92371f1bbc55663
MrBlueBlobGuy <aternoshostingmc@gmail.com>
2024-10-30 00:13:05 +0530

    Added new page

---
+++ pages/newpage.js
@@ -0,0 +1,11 @@
+// pages/newpage.js
+import React from 'react';
+
+export default function NewPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-8">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">This is the New Page</h1>
+      <p className="text-lg text-gray-700">Welcome to your new page in Next.js styled with Tailwind CSS!</p>
+    </div>
+  );
+}
