9a60c93e06172654a6971ef4bf36e9efdbe41419
Vineethaitha <vineethaitha1234@gmail.com>
2024-10-29 23:01:28 +0530

    Added Vineeth and link from index.js

---
+++ pages/Vineeth.js
@@ -0,0 +1,12 @@
+// src/YourPage.js
+import React from 'react';
+
+const Vineeth = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold">Welcome to Your Page</h1>
+        </div>
+    );
+};
+
+export default Vineeth;
