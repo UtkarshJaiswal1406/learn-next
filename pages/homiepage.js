e15d387ea90b776606da8c9bf68966146afa8d42
RichieRichard0000 <devanshb1627@gmail.com>
2024-10-29 20:41:13 +0530

    Added new page and link/button to navigate to it

---
+++ pages/homiepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const homiepage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Welcome to Homie Page!</h1>
+    </div>
+  );
+};
+
+export default homiepage;
