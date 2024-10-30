0e3da33f79f9af0842cf67b4eebcfc286ea29d2d
vaisaliOH <vaisalinair05@gmail.com>
2024-10-30 20:18:36 +0530

    added changes

--- pages/page.js
+++ pages/page.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const MyPage = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-blue-50">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-blue-50">
+      <img 
+        src="/pic.jpg"
+        alt="Description of image"
+        className="w-1/3 h-auto rounded-lg shadow-lg border-4 border-blue-500 shadow-xl mb-4 hover:scale-105 hover:shadow-2xl transition-all duration-300"
+      />
       <h1 className="text-4xl font-bold text-blue-500">Welcome to My Page!</h1>
+      <p className="text-lg text-gray-500 font-medium italic tracking-wide hover:text-blue-600 transition-colors duration-200">
+  HOW IS THE TASK??
+</p>
+
+
     </div>
   );
 };
