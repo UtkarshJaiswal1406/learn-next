c368bc4c343ea593c97cf96edb93753f7c0806b8
pallavi <pallavi6315@gmail.com>
2024-10-30 14:43:57 +0530

    Create newWebpage.js

---
+++ pages/newWebpage.js
@@ -0,0 +1,9 @@
+
+export default function NewPage() {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-3xl">Hallo, ich heiße Pallavi!Wie heißt du</h1>
+      </div>
+    );
+  }
+  
